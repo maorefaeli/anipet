161e374bad96518a7ed35db9e97891dff1937bad
Maor Refaeli <14859752+maorefaeli@users.noreply.github.com>
2019-08-09 19:31:10 +0300

    Add basic controllers and view for model

---
+++ anipet/Controllers/FoodSourcesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using anipet.Models;
+
+namespace anipet.Controllers
+{
+    public class FoodSourcesController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: FoodSources
+        public ActionResult Index()
+        {
+            return View(db.FoodSources.ToList());
+        }
+
+        // GET: FoodSources/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FoodSource foodSource = db.FoodSources.Find(id);
+            if (foodSource == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foodSource);
+        }
+
+        // GET: FoodSources/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FoodSources/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,SourcePricePerKilo")] FoodSource foodSource)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FoodSources.Add(foodSource);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(foodSource);
+        }
+
+        // GET: FoodSources/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FoodSource foodSource = db.FoodSources.Find(id);
+            if (foodSource == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foodSource);
+        }
+
+        // POST: FoodSources/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,SourcePricePerKilo")] FoodSource foodSource)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(foodSource).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(foodSource);
+        }
+
+        // GET: FoodSources/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FoodSource foodSource = db.FoodSources.Find(id);
+            if (foodSource == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foodSource);
+        }
+
+        // POST: FoodSources/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FoodSource foodSource = db.FoodSources.Find(id);
+            db.FoodSources.Remove(foodSource);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ anipet/Controllers/PurchasesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using anipet.Models;
+
+namespace anipet.Controllers
+{
+    public class PurchasesController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Purchases
+        public ActionResult Index()
+        {
+            return View(db.Purchases.ToList());
+        }
+
+        // GET: Purchases/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchases.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // GET: Purchases/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Purchases/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,PurchaseId,Date")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Purchases.Add(purchase);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(purchase);
+        }
+
+        // GET: Purchases/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchases.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // POST: Purchases/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,PurchaseId,Date")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(purchase).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(purchase);
+        }
+
+        // GET: Purchases/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchases.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // POST: Purchases/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Purchase purchase = db.Purchases.Find(id);
+            db.Purchases.Remove(purchase);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
