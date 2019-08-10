fb3cf9a19797bf9bd19de5cf0e0c0a4ef3099c8b
Guy <guy.sl10@gmail.com>
2019-08-10 12:52:35 +0300

    adding user controller and basic views

--- anipet/Controllers/UsersController.cs
+++ anipet/Controllers/UsersController.cs
@@ -17,8 +17,55 @@ namespace anipet.Controllers
         // GET: Users
         public ActionResult Index()
         {
-            return View(db.Users.ToList());
+            var currentUser = ((User)HttpContext.Session["user"]);
+            if (currentUser != null)
+            {
+                var users = db.Users.Select(s => s);
+                if (!currentUser.IsAdmin)
+                {
+                    users = users.Where(u => u.Id == currentUser.Id);
+                }
+                return View(users.Include(r => r.Stores).ToList());
+            }
+            return RedirectToAction("Index", "Error");
         }
+
+        // GET: Users/Search
+        public ActionResult Search(string username = null, int minStores = 0, string IsAdmin = "dont filter")
+        {
+            var currentUser = ((User)HttpContext.Session["user"]);
+            if (currentUser != null)
+            {
+                if (currentUser.IsAdmin)
+                {
+                    var returnDataQurey = db.Users.Include(r => r.Stores);
+
+                    if (!string.IsNullOrEmpty(username))
+                    {
+                        returnDataQurey = returnDataQurey.Where(r => r.Username.Contains(username));
+                    }
+
+                    if (IsAdmin != "dont filter")
+                    {
+                        bool admin = IsAdmin.Equals("True");
+                        returnDataQurey = returnDataQurey.Where(r => r.IsAdmin == admin);
+                    }
+
+                    if (minStores > 0)
+                    {
+                        returnDataQurey = returnDataQurey.Where(r => r.Stores.Count() >= minStores);
+                    }
+
+                    return View("Index", returnDataQurey.ToList());
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Error", new { message = "you are not autorized" });
+                }
+            }
+            return RedirectToAction("Index", "Error");
+        }
+
 
         // GET: Users/Details/5
         public ActionResult Details(int? id)
@@ -28,6 +75,7 @@ namespace anipet.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             User user = db.Users.Find(id);
+            ViewBag.userStoreList = db.Stores.Where(r => r.StoreAdmin.Id == id).ToList();
             if (user == null)
             {
                 return HttpNotFound();
@@ -38,8 +86,48 @@ namespace anipet.Controllers
         // GET: Users/Create
         public ActionResult Create()
         {
+            var currentUser = ((User)HttpContext.Session["user"]);
+            if (currentUser == null)
+            {
+                return View();
+            }
+            else
+            {
+                if (currentUser.IsAdmin)
+                {
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Error", new { message = "you are already logged in" });
+                }
+            }
+        }
+
+        // GET: Users/Login
+        public ActionResult Login()
+        {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string userName, string password)
+        {
+            var userInDataBase = db.Users.Include(r => r.FavoriteProduct).Where(s =>
+                        s.Username.Equals(userName, System.StringComparison.Ordinal) &&
+                        s.Password.Equals(password, System.StringComparison.Ordinal)).SingleOrDefault();
+            if (userInDataBase != null)
+            {
+                System.Web.HttpContext.Current.Session["user"] = userInDataBase;
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ErrMsg = "User name or password are incorrect.";
+            return View();
+        }
+
+
 
         // POST: Users/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
@@ -50,11 +138,25 @@ namespace anipet.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Users.Add(user);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (db.Users.Where(c => c.Username.Equals(user.Username)).Count() > 0)
+                {
+                    ViewBag.ErrMsg = "User name already exists, please try again";
+                }
+                else
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    if (System.Web.HttpContext.Current.Session["user"] == null)
+                    {
+                        System.Web.HttpContext.Current.Session["user"] = user;
+                        return RedirectToAction("Index", "Stores");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
             }
-
             return View(user);
         }
 
@@ -69,6 +171,10 @@ namespace anipet.Controllers
             if (user == null)
             {
                 return HttpNotFound();
+            }
+            if (((User)System.Web.HttpContext.Current.Session["user"]).Id != id && ((User)System.Web.HttpContext.Current.Session["user"]).IsAdmin == false)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             return View(user);
         }
@@ -109,10 +215,38 @@ namespace anipet.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
+            var userInDataBase = db.Users.Where(s =>
+                       s.Id == id).SingleOrDefault();
+
             User user = db.Users.Find(id);
-            db.Users.Remove(user);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            try
+            {
+                db.Users.Remove(user);
+                db.SaveChanges();
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Error", new { message = "ERROR - You are tring to delete user who owns one or more stores" });
+            }
+
+            if (userInDataBase != null)
+            {
+                if (((User)(System.Web.HttpContext.Current.Session["user"])).IsAdmin)
+                    return RedirectToAction("Index");
+                else
+                {
+                    System.Web.HttpContext.Current.Session["user"] = null;
+                    return RedirectToAction("Login");
+                }
+            }
+            ViewBag.ErrMsg = "User does not exist.";
+            return View();
+        }
+
+        public ActionResult Logoff()
+        {
+            System.Web.HttpContext.Current.Session["user"] = null;
+            return RedirectToAction("Login");
         }
 
         protected override void Dispose(bool disposing)
