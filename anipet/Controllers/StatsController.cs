6336c1f60ac7d730b53ae5d9012aa76980202f7e
AlonAshkenazi1 <alon0700@gmail.com>
2019-08-10 14:32:13 +0300

    stats controller

---
+++ anipet/Controllers/StatsController.cs
@@ -0,0 +1,32 @@
+﻿using anipet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace anipet.Controllers
+{
+    public class StatsController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        [ChildActionOnly]
+        public PartialViewResult getStats()
+        {
+            var ProductByFoodSource = db.Products.GroupBy(r => r.FoodSource).Select(g => new { foodSource = ((FoodSource)g.Key).Name, count = g.Count() }).ToList();
+            var ProductByFoodSourceJSON = JsonConvert.SerializeObject(ProductByFoodSource);
+            ViewBag.ProductByFoodSource = ProductByFoodSourceJSON;
+
+            var ProductByStore = db.Stores.Select(r => new { store = r.City + "," + r.StreetAddress, count = r.Products.Count() }).ToList();
+            var ProductByStoreJSON = JsonConvert.SerializeObject(ProductByStore);
+            ViewBag.ProductByStore = ProductByStoreJSON;
+
+            var StoresByCity = db.Stores.GroupBy(r => r.City).Select(g => new { city = g.Key.ToString(), count = g.Count() }).ToList();
+            var StoresByCityJSON = JsonConvert.SerializeObject(ProductByFoodSource);
+            ViewBag.StoresByCityJSON = StoresByCityJSON;
+
+            return PartialView("~/Views/Stats/Stats.cshtml");
+        }
+    }
+}
