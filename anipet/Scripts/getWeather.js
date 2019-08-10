e798f4073a9beca8f267d3a2c34d6a22479f2f99
Shai <shailieberman@gmail.com>
2019-08-10 16:43:44 +0300

    home page is gorgeous

---
+++ anipet/Scripts/getWeather.js
@@ -0,0 +1,15 @@
+﻿var queryURL = "https://openweathermap.org/data/2.5/weather?q=Tel%20Aviv%20District&appid=b6907d289e10d714a6e88b30761fae22";
+
+$.getJSON(queryURL, function (data) {
+    var temp = Math.floor(data.main.temp_max);
+
+    $("#temperatureDiv p").html(temp + " degrees");
+
+    if (temp >= 30) {
+        $("#temperatureDiv").append("<img class=\"tempartureImg\" src=\"/content/Images/dog-sweating.png\">");
+    } else if (temp >= 18) {
+        $("#temperatureDiv").append("<img class=\"tempartureImg\" src=\"/content/Images/cool-dog.png\">");
+    } else {
+        $("#temperatureDiv").append("<img class=\"tempartureImg\" src=\"/content/Images/cold-dog.png\">");
+    }
+})
---
+++ anipet/Scripts/weather.js
@@ -0,0 +1,18 @@
+﻿var weatherCallback = function (data) {
+    var wind = data.query.results.channel.wind;
+    var item = data.query.results.channel.item;
+    var icon = "<i class='material-icons'>";
+
+    if (item.condition.temp <= 20) {
+        icon += "wb_cloudy</i >";
+    }
+    else if (item.condition.temp > 20 && item.condition.temp < 28) {
+        icon += "wb_incandescent</i >";
+    } else {
+        icon += "wb_sunny</i >";
+    }
+
+    var text = icon + "&nbsp;&nbsp;Temperature: " + item.condition.temp + " °C";
+
+    $("#temperatureDiv p").html(text);
+};
