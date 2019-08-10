d0faf9e139422adf9b6fa11ed1d1ab9312b50b09
AlonAshkenazi1 <alon0700@gmail.com>
2019-08-10 12:48:56 +0300

    Home controller and view

--- anipet/Controllers/HomeController.cs
+++ anipet/Controllers/HomeController.cs
@@ -8,8 +8,10 @@ namespace anipet.Controllers
 {
     public class HomeController : Controller
     {
+        private DBContext db = new DBContext();
         public ActionResult Index()
         {
+            ViewBag.mostPopProduct = db.Users.GroupBy(r => r.FavoriteProduct).Select(g => new { Product = ((Product)g.Key), count = g.Count() }).OrderByDescending(r => r.count).First().Product;
             return View();
         }
 
@@ -20,11 +22,23 @@ namespace anipet.Controllers
             return View();
         }
 
+        public string GetAboutData()
+        {
+            var text = System.IO.File.ReadAllText(Server.MapPath("\\Content\\About\\aboutData.txt"));
+            return text;
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        public ActionResult Purchases()
+        {
+            return View();
+        }
     }
+
 }
